d25d5f8b13e5c2d802d64cd347cde287b4aae1c8
Tigran <tigranv@10web.io>
2021-04-12 18:33:16 +0400

    Code Quality Options

--- typescript compiler and it's configuration/src/app.ts
+++ typescript compiler and it's configuration/src/app.ts
@@ -1,10 +1,18 @@
 const button = document.querySelector('button');
 
-function clickHandler() {
-    console.log('Clicked!')
+// function add(n1: number, n2: number):string { //A function whose declared type is neither 'void' nor 'any' must return a value.
+//     if (false) {
+//         return n1.toString() + n2.toString()
+//     }
+//     // return
+// }
+
+function clickHandler(message: string, age: number) {
+    // let userName = message
+    console.log('Clicked!' + message)
 }
 
 // a comment
-if( button ) {
-    button.addEventListener('click', clickHandler.bind(null))
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, "hello", 1))
 }
