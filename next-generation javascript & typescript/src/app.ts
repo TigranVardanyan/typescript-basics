3506383f4e1676ed707a6e0d1ede2e8af400a075
Tigran <tigranv@10web.io>
2021-04-12 18:47:50 +0400

    "let" and "const"

---
+++ next-generation javascript & typescript/src/app.ts
@@ -0,0 +1,28 @@
+const userName = 'Tigran';
+// userName = 'Tigrann';
+let age = 30;
+age++;
+
+var result;
+
+function add(a:number, b:number) {
+    var result;
+    result = a + b;
+    return result
+}
+
+// console.log(result); // Cannot find name 'result'.
+// var has function and global scope
+// let has additional block scope
+
+// let myAge = 24;
+
+console.log(result)
+
+// if (age) {
+//     let myAge = 23
+//
+// }
+//
+// console.log(myAge)
+
