a2d838e78aa3a71f52af10a2ebfce0267f32dfe3
Tigran <tigranv@10web.io>
2021-05-18 19:50:36 +0400

    Access Modifiers

--- classes and interfaces/src/app.js
+++ classes and interfaces/src/app.js
@@ -1,13 +1,29 @@
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log('Department: ' + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var plugin_team = new Department("Plugin team");
 plugin_team.describe();
-var plugin_team_copy = { name: 's', describe: plugin_team.describe };
-plugin_team_copy.describe();
+plugin_team.addEmployee('Hakob');
+plugin_team.addEmployee('Tigran');
+plugin_team.addEmployee('Karen');
+plugin_team.addEmployee('Anahit');
+plugin_team.addEmployee('Tigran');
+// plugin_team.employees[7] = "Nik" // wrong way
+plugin_team.printEmployeeInformation();
+// const plugin_team_copy = {name: 's', describe: plugin_team.describe}
+//
+// plugin_team_copy.describe();
--- classes and interfaces/src/app.ts
+++ classes and interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string;
+    private employees: string[] = []
 
     constructor(n: string) {
         this.name = n;
@@ -8,12 +9,29 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name)
     }
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
 const plugin_team = new Department("Plugin team")
 
 plugin_team.describe()
 
-const plugin_team_copy = {name: 's', describe: plugin_team.describe}
+plugin_team.addEmployee('Hakob');
+plugin_team.addEmployee('Tigran');
+plugin_team.addEmployee('Karen');
+plugin_team.addEmployee('Anahit');
+plugin_team.addEmployee('Tigran');
 
-plugin_team_copy.describe();
+// plugin_team.employees[7] = "Nik" // wrong way
+
+plugin_team.printEmployeeInformation();
+
+// const plugin_team_copy = {name: 's', describe: plugin_team.describe}
+//
+// plugin_team_copy.describe();
