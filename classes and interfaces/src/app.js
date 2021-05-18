e7c8dcb432324d89caf813d79a536175bd4f038e
Tigran <tigranv@10web.io>
2021-05-18 19:43:50 +0400

    Constructor Functions & The "this" Keyword

--- classes and interfaces/src/app.js
+++ classes and interfaces/src/app.js
@@ -2,7 +2,12 @@ var Department = /** @class */ (function () {
     function Department(n) {
         this.name = n;
     }
+    Department.prototype.describe = function () {
+        console.log('Department: ' + this.name);
+    };
     return Department;
 }());
 var plugin_team = new Department("Plugin team");
-console.log(plugin_team);
+plugin_team.describe();
+var plugin_team_copy = { name: 's', describe: plugin_team.describe };
+plugin_team_copy.describe();
--- classes and interfaces/src/app.ts
+++ classes and interfaces/src/app.ts
@@ -1,10 +1,19 @@
 class Department {
     name: string;
-    constructor(n:string) {
+
+    constructor(n: string) {
         this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log('Department: ' + this.name)
     }
 }
 
 const plugin_team = new Department("Plugin team")
 
-console.log(plugin_team)
+plugin_team.describe()
+
+const plugin_team_copy = {name: 's', describe: plugin_team.describe}
+
+plugin_team_copy.describe();
