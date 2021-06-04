140fdd0933ae025687ae4d0614b13c3a41a70706
Tigran <tig.vardanyan.001@gmail.com>
2021-06-04 16:42:01 +0400

    More on Type Guards

--- advanced_types/src/app.js
+++ advanced_types/src/app.js
@@ -9,9 +9,49 @@
 // }
 //
 // type ElevatedEmployee = Admin & Employee;
-;
 const el = {
     name: "Max",
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    // console.log("Name: " + emp.privileges) // not always emp has privileges field
+    if ('privileges' in emp) {
+        console.log("Name: " + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log("StartDate: " + emp.startDate);
+    }
+}
+printEmployeeInformation(el);
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving...");
+    }
+    loadCargo(amount) {
+        console.log("Loading amount: " + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicles(vehicles) {
+    vehicles.drive();
+    // if ('loadCargo' in vehicles) {
+    //     vehicles.loadCargo(100)
+    // }
+    if (vehicles instanceof Truck) {
+        vehicles.loadCargo(1000);
+    }
+}
--- advanced_types/src/app.ts
+++ advanced_types/src/app.ts
@@ -21,8 +21,7 @@ interface Employee {
 }
 
 interface ElevatedEmployee extends Admin, Employee {
-};
-
+}
 
 const el: ElevatedEmployee = {
     name: "Max",
@@ -34,3 +33,58 @@ type Combinable = string | number;
 type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic;
+
+function add(a:Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string' ) {
+        return a.toString() + b.toString();
+    }
+    return a + b
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log("Name: " + emp.name)
+    // console.log("Name: " + emp.privileges) // not always emp has privileges field
+    if ('privileges' in emp) {
+        console.log("Name: " + emp.privileges)
+    }
+    if ('startDate' in emp) {
+        console.log("StartDate: " + emp.startDate)
+    }
+
+}
+
+printEmployeeInformation(el)
+
+class Car {
+    drive() {
+        console.log("Driving...")
+    }
+}
+
+class Truck {
+    drive() {
+        console.log("Driving...")
+    }
+
+    loadCargo(amount:number) {
+        console.log("Loading amount: " + amount)
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car()
+const v2 = new Truck()
+
+
+function useVehicles(vehicles: Vehicle) {
+    vehicles.drive()
+    // if ('loadCargo' in vehicles) {
+    //     vehicles.loadCargo(100)
+    // }
+    if (vehicles instanceof Truck) {
+        vehicles.loadCargo(1000)
+    }
+}
