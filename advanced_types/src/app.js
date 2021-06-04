4a99ec917b2c0d6014670ff92c01fb52d6983e89
Tigran <tigranv@10web.io>
2021-06-04 16:28:19 +0400

    Intersection Types

---
+++ advanced_types/src/app.js
@@ -0,0 +1,17 @@
+// type Admin = {
+//     name: string;
+//     privileges: string[];
+// }
+//
+// type Employee = {
+//     name: string;
+//     startDate: Date;
+// }
+//
+// type ElevatedEmployee = Admin & Employee;
+;
+const el = {
+    name: "Max",
+    privileges: ['create-server'],
+    startDate: new Date()
+};
---
+++ advanced_types/src/app.ts
@@ -0,0 +1,35 @@
+// type Admin = {
+//     name: string;
+//     privileges: string[];
+// }
+//
+// type Employee = {
+//     name: string;
+//     startDate: Date;
+// }
+//
+// type ElevatedEmployee = Admin & Employee;
+
+interface Admin {
+    name: string;
+    privileges: string[];
+}
+
+interface Employee {
+    name: string;
+    startDate: Date;
+}
+
+interface ElevatedEmployee extends Admin,Employee{};
+
+
+const el: ElevatedEmployee = {
+    name: "Max",
+    privileges: ['create-server'],
+    startDate: new Date()
+}
+
+type Combinable = string | number;
+type Numberic = number | boolean;
+
+type Universal = Combinable & Numberic;
