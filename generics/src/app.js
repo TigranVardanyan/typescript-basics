b35fc6a6f57765df44f80f2e83d7e7b9b07ebb1d
Tigran <tig.vardanyan.001@gmail.com>
2021-06-08 19:25:55 +0400

    Built-in Generics & What are Generics

--- generics/src/app.js
+++ generics/src/app.js
@@ -1,2 +1,13 @@
-// Code goes here!
-console.log('hello world');
+// const names = ['tigran', 'anna'];
+// const anyArray = [];
+const names = [];
+const ages = [];
+const promice = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 2000);
+});
+promice.then(data => {
+    // data.split(' ')
+    console.log(data);
+});
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,2 +1,17 @@
-// Code goes here!
-console.log('hello world');
+// const names = ['tigran', 'anna'];
+// const anyArray = [];
+
+const names: Array<string> = [];
+
+const ages: Array<number> = [];
+
+const promice: Promise<number> = new Promise<number>((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10)
+    }, 2000)
+})
+
+promice.then(data => {
+    // data.split(' ')
+    console.log(data)
+})
