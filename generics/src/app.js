4e6c071fef66eda3ec7191989474c11612cd24d6
Tigran <tig.vardanyan.001@gmail.com>
2021-06-08 19:37:57 +0400

    Creating a Generic Function

--- generics/src/app.js
+++ generics/src/app.js
@@ -1,13 +1,22 @@
 // const names = ['tigran', 'anna'];
 // const anyArray = [];
-const names = [];
-const ages = [];
-const promice = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(10);
-    }, 2000);
-});
-promice.then(data => {
-    // data.split(' ')
-    console.log(data);
-});
+// const names: Array<string> = [];
+//
+// const ages: Array<number> = [];
+//
+// const promise: Promise<number> = new Promise<number>((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10)
+//     }, 2000)
+// })
+//
+// promice.then(data => {
+//     // data.split(' ')
+//     console.log(data)
+// })
+function merge(obj1, obj2) {
+    return Object.assign(obj1, obj2);
+}
+// console.log(merge({name: "Tigran"}, {age:23}))
+const mergedObj = merge({ name: "Tigran" }, { age: 23 });
+console.log(mergedObj.name);
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,17 +1,29 @@
 // const names = ['tigran', 'anna'];
 // const anyArray = [];
 
-const names: Array<string> = [];
+// const names: Array<string> = [];
+//
+// const ages: Array<number> = [];
+//
+// const promise: Promise<number> = new Promise<number>((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10)
+//     }, 2000)
+// })
+//
+// promice.then(data => {
+//     // data.split(' ')
+//     console.log(data)
+// })
 
-const ages: Array<number> = [];
 
-const promice: Promise<number> = new Promise<number>((resolve, reject) => {
-    setTimeout(() => {
-        resolve(10)
-    }, 2000)
-})
 
-promice.then(data => {
-    // data.split(' ')
-    console.log(data)
-})
+function merge<T, U>(obj1: T, obj2: U): T & U {
+    return Object.assign(obj1, obj2)
+}
+
+// console.log(merge({name: "Tigran"}, {age:23}))
+
+const mergedObj = merge({name: "Tigran"}, {age:23})
+
+console.log(mergedObj.name);
