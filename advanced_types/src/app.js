95e96ec6b43a98e78c5ec438a02d5b8b5fc1121d
Tigran <tig.vardanyan.001@gmail.com>
2021-06-08 18:31:46 +0400

    Function Overloads

--- advanced_types/src/app.js
+++ advanced_types/src/app.js
@@ -20,67 +20,106 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log("Name: " + emp.name);
-    // console.log("Name: " + emp.privileges) // not always emp has privileges field
-    if ('privileges' in emp) {
-        console.log("Name: " + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log("StartDate: " + emp.startDate);
-    }
-}
-printEmployeeInformation(el);
-class Car {
-    drive() {
-        console.log("Driving...");
-    }
-}
-class Truck {
-    drive() {
-        console.log("Driving...");
-    }
-    loadCargo(amount) {
-        console.log("Loading amount: " + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicles(vehicles) {
-    vehicles.drive();
-    // if ('loadCargo' in vehicles) {
-    //     vehicles.loadCargo(100)
-    // }
-    if (vehicles instanceof Truck) {
-        vehicles.loadCargo(1000);
-    }
-}
-useVehicles(v1);
-useVehicles(v2);
-function moveAnimal(animal) {
-    let speed;
-    // if ('flyingSpeed' in animal) {
-    //     console.log('Moving with speed: ' + animal.flyingSpeed)
-    // }
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log('Moving with speed: ' + speed);
-}
-moveAnimal({ type: "bird", flyingSpeed: 10 });
-moveAnimal({ type: "horse", runningSpeed: 15 });
-const paragraph = document.getElementById('message-output');
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!  conflict with react
-// ! not need with type casting \|/
-const userInputElement = document.getElementById('user-input');
-userInputElement.value = 'hi';
-console.log(paragraph);
-const errorBag = {
-    email: 'Not a valid email',
-    username: 'Must start with a capital character'
-};
+const result = add('Tigran', 'Vardanyan');
+// result.split(' ')  error result is type Combinable!!!
+//after function overload if work
+result.split(' ');
+const numberResult = add(5, 10);
+// numberResult.split(' ') typescript know that is number
+// type UnknownEmployee = Employee | Admin;
+//
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//     console.log("Name: " + emp.name)
+//     // console.log("Name: " + emp.privileges) // not always emp has privileges field
+//     if ('privileges' in emp) {
+//         console.log("Name: " + emp.privileges)
+//     }
+//     if ('startDate' in emp) {
+//         console.log("StartDate: " + emp.startDate)
+//     }
+//
+// }
+//
+// printEmployeeInformation(el)
+//
+// class Car {
+//     drive() {
+//         console.log("Driving...")
+//     }
+// }
+//
+// class Truck {
+//     drive() {
+//         console.log("Driving...")
+//     }
+//
+//     loadCargo(amount:number) {
+//         console.log("Loading amount: " + amount)
+//     }
+// }
+//
+// type Vehicle = Car | Truck;
+//
+// const v1 = new Car()
+// const v2 = new Truck()
+//
+//
+// function useVehicles(vehicles: Vehicle) {
+//     vehicles.drive()
+//     // if ('loadCargo' in vehicles) {
+//     //     vehicles.loadCargo(100)
+//     // }
+//     if (vehicles instanceof Truck) {
+//         vehicles.loadCargo(1000)
+//     }
+// }
+//
+// useVehicles(v1)
+// useVehicles(v2)
+//
+// interface Bird {
+//     type: 'bird',
+//     flyingSpeed:number
+// }
+//
+// interface Horse {
+//     type: 'horse',
+//     runningSpeed:number
+// }
+//
+// type Animal = Bird | Horse
+//
+// function moveAnimal(animal: Animal) {
+//     let speed;
+//     // if ('flyingSpeed' in animal) {
+//     //     console.log('Moving with speed: ' + animal.flyingSpeed)
+//     // }
+//     switch (animal.type) {
+//         case "bird":
+//             speed = animal.flyingSpeed
+//             break
+//         case "horse":
+//             speed = animal.runningSpeed
+//             break
+//     }
+//     console.log('Moving with speed: ' + speed)
+// }
+//
+// moveAnimal({type:"bird", flyingSpeed:10})
+// moveAnimal({type:"horse", runningSpeed:15})
+//
+// const paragraph  = document.getElementById('message-output')
+// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!  conflict with react
+// // ! not need with type casting \|/
+// const userInputElement = document.getElementById('user-input') as HTMLInputElement;
+// userInputElement.value = 'hi'
+// console.log(paragraph)
+//
+// interface ErrorContainer { //{email:'not a valid email, username: 'Must start with character'}
+//     [prop:string]:string
+// }
+//
+// const errorBag: ErrorContainer = {
+//     email: 'Not a valid email',
+//     username: 'Must start with a capital character'
+// }
--- advanced_types/src/app.ts
+++ advanced_types/src/app.ts
@@ -34,6 +34,10 @@ type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic;
 
+function add(a:number, b:number):number;
+function add(a:string, b:string):string;
+function add(a:string, b:number):string;
+function add(a:number, b:number):string;
 function add(a:Combinable, b: Combinable) {
     if (typeof a === 'string' || typeof b === 'string' ) {
         return a.toString() + b.toString();
@@ -41,100 +45,112 @@ function add(a:Combinable, b: Combinable) {
     return a + b
 }
 
-type UnknownEmployee = Employee | Admin;
-
-function printEmployeeInformation(emp: UnknownEmployee) {
-    console.log("Name: " + emp.name)
-    // console.log("Name: " + emp.privileges) // not always emp has privileges field
-    if ('privileges' in emp) {
-        console.log("Name: " + emp.privileges)
-    }
-    if ('startDate' in emp) {
-        console.log("StartDate: " + emp.startDate)
-    }
-
-}
-
-printEmployeeInformation(el)
-
-class Car {
-    drive() {
-        console.log("Driving...")
-    }
-}
-
-class Truck {
-    drive() {
-        console.log("Driving...")
-    }
-
-    loadCargo(amount:number) {
-        console.log("Loading amount: " + amount)
-    }
-}
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car()
-const v2 = new Truck()
+const result = add('Tigran', 'Vardanyan')
+// result.split(' ')  error result is type Combinable!!!
 
 
-function useVehicles(vehicles: Vehicle) {
-    vehicles.drive()
-    // if ('loadCargo' in vehicles) {
-    //     vehicles.loadCargo(100)
-    // }
-    if (vehicles instanceof Truck) {
-        vehicles.loadCargo(1000)
-    }
-}
+//after function overload if work
+result.split(' ')
 
-useVehicles(v1)
-useVehicles(v2)
+const numberResult = add(5,10);
 
-interface Bird {
-    type: 'bird',
-    flyingSpeed:number
-}
+// numberResult.split(' ') typescript know that is number
 
-interface Horse {
-    type: 'horse',
-    runningSpeed:number
-}
 
-type Animal = Bird | Horse
-
-function moveAnimal(animal: Animal) {
-    let speed;
-    // if ('flyingSpeed' in animal) {
-    //     console.log('Moving with speed: ' + animal.flyingSpeed)
-    // }
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed
-            break
-        case "horse":
-            speed = animal.runningSpeed
-            break
-    }
-    console.log('Moving with speed: ' + speed)
-}
-
-moveAnimal({type:"bird", flyingSpeed:10})
-moveAnimal({type:"horse", runningSpeed:15})
-
-const paragraph  = document.getElementById('message-output')
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!  conflict with react
-// ! not need with type casting \|/
-const userInputElement = document.getElementById('user-input') as HTMLInputElement;
-userInputElement.value = 'hi'
-console.log(paragraph)
-
-interface ErrorContainer { //{email:'not a valid email, username: 'Must start with character'}
-    [prop:string]:string
-}
-
-const errorBag: ErrorContainer = {
-    email: 'Not a valid email',
-    username: 'Must start with a capital character'
-}
+// type UnknownEmployee = Employee | Admin;
+//
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//     console.log("Name: " + emp.name)
+//     // console.log("Name: " + emp.privileges) // not always emp has privileges field
+//     if ('privileges' in emp) {
+//         console.log("Name: " + emp.privileges)
+//     }
+//     if ('startDate' in emp) {
+//         console.log("StartDate: " + emp.startDate)
+//     }
+//
+// }
+//
+// printEmployeeInformation(el)
+//
+// class Car {
+//     drive() {
+//         console.log("Driving...")
+//     }
+// }
+//
+// class Truck {
+//     drive() {
+//         console.log("Driving...")
+//     }
+//
+//     loadCargo(amount:number) {
+//         console.log("Loading amount: " + amount)
+//     }
+// }
+//
+// type Vehicle = Car | Truck;
+//
+// const v1 = new Car()
+// const v2 = new Truck()
+//
+//
+// function useVehicles(vehicles: Vehicle) {
+//     vehicles.drive()
+//     // if ('loadCargo' in vehicles) {
+//     //     vehicles.loadCargo(100)
+//     // }
+//     if (vehicles instanceof Truck) {
+//         vehicles.loadCargo(1000)
+//     }
+// }
+//
+// useVehicles(v1)
+// useVehicles(v2)
+//
+// interface Bird {
+//     type: 'bird',
+//     flyingSpeed:number
+// }
+//
+// interface Horse {
+//     type: 'horse',
+//     runningSpeed:number
+// }
+//
+// type Animal = Bird | Horse
+//
+// function moveAnimal(animal: Animal) {
+//     let speed;
+//     // if ('flyingSpeed' in animal) {
+//     //     console.log('Moving with speed: ' + animal.flyingSpeed)
+//     // }
+//     switch (animal.type) {
+//         case "bird":
+//             speed = animal.flyingSpeed
+//             break
+//         case "horse":
+//             speed = animal.runningSpeed
+//             break
+//     }
+//     console.log('Moving with speed: ' + speed)
+// }
+//
+// moveAnimal({type:"bird", flyingSpeed:10})
+// moveAnimal({type:"horse", runningSpeed:15})
+//
+// const paragraph  = document.getElementById('message-output')
+// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!  conflict with react
+// // ! not need with type casting \|/
+// const userInputElement = document.getElementById('user-input') as HTMLInputElement;
+// userInputElement.value = 'hi'
+// console.log(paragraph)
+//
+// interface ErrorContainer { //{email:'not a valid email, username: 'Must start with character'}
+//     [prop:string]:string
+// }
+//
+// const errorBag: ErrorContainer = {
+//     email: 'Not a valid email',
+//     username: 'Must start with a capital character'
+// }
